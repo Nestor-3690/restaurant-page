3c60bad55a1dd42f50db7c8c7ba5b935baa1642e
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-06-03 14:04:37 +0200

    Add images and styles for the pages

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     entry: './src/index.js',
     devServer: {
         static: './dist',
-        watchFiles: ['./src/index.html'],
+        watchFiles: ['./src/index.html', './restaurant-outside.jpg'],
     },
     plugins: [
         new HtmlWebpackPlugin({
@@ -23,6 +23,14 @@ module.exports = {
             {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
             },
         ],
     },
